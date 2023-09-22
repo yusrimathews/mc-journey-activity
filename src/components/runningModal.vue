1a35af18447de04e1fd32ed66c7c34787e546ebf
Yusri <yusrimathews@gmail.com>
2023-09-22 17:11:52 +0700

    Updated running hover & modal

--- src/components/runningModal.vue
+++ src/components/runningModal.vue
@@ -7,12 +7,15 @@
       <span class="tag">Debug</span>
       <span class="tag is-warning is-light">State</span>
     </div>
-    <pre>{{ [this.$data, this.$store.state] }}</pre>
+    <pre>{{ this.$store.state }}</pre>
   </section>
 </template>
 
 <script>
+import customActivity from '@/mixins/customActivity';
+
 export default {
-  name: 'Modal'
+  name: 'Modal',
+  mixins: [customActivity]
 }
 </script>

4c56e810a298ff5757110b46d621983cf587516f
Yusri <yusrimathews@gmail.com>
2023-09-22 17:10:24 +0700

    Installed vue logger plugin

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import logger from './logger';
 import router from './router';
 import store from './store';
 
 const app = createApp(App)
+  .use(logger)
   .use(router)
   .use(store);
 

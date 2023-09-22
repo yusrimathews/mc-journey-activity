0b63e0ef5fc1fae64bfb94f772eca4a3d0a78ef4
Yusri <yusrimathews@gmail.com>
2023-09-22 11:01:10 +0700

    Simplify logger and cosmetics

--- routes/publish.js
+++ routes/publish.js
@@ -3,11 +3,11 @@ const logger = require('../lib/logger');
 
 module.exports = (req, res) => {
   try {
-    logger.info(`[publish.js] ${JSON.stringify(req)}`);
+    logger.info('[publish.js] success :', req.body);
 
     res.json({ success: true });
   } catch (error) {
-    logger.error(`[publish.js] ${JSON.stringify(error)}`);
+    logger.error('[publish.js] error :', error);
 
     res.status(500).json({ error: true });
   }
--- routes/validate.js
+++ routes/validate.js
@@ -2,11 +2,11 @@ const logger = require('../lib/logger');
 
 module.exports = (req, res) => {
   try {
-    logger.info(`[validate.js] ${JSON.stringify(req)}`);
-
+    logger.info('[validate.js] success :', req.body);
+    
     res.json({ success: true });
   } catch (error) {
-    logger.error(`[validate.js] ${JSON.stringify(error)}`);
+    logger.error('[validate.js] error :', error);
 
     res.status(500).json({ error: true });
   }

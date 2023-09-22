37f8f61cc93d57cd31c41c63f966942949b5cb52
Yusri <yusrimathews@gmail.com>
2023-09-22 17:09:57 +0700

    Refactored app.js, installed nocache

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
-const history = require('connect-history-api-fallback');
 const app = express();
 const logger = require('./lib/logger');
 
-// Configure parsers
-app.use(cors());
-app.use(history());
+// Optional environment variables
+const port = process.env.PORT || 8081;
+
+// Configure middleware & parsers
+app.use(require('cors'));
+app.use(require('connect-history-api-fallback'));
+app.use(require('nocache'));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(require('body-parser').raw({
-//   type: 'application/jwt'
-// }));
+app.use(express.urlencoded({ extended: true }));
+app.use(require('body-parser').raw({
+  type: 'application/jwt'
+}));
+app.disable('x-powered-by');
 
-// Configure headers
-// app.use(function (req, res, next) {
-//   res.set('Cache-control', 'no-store');
-//   res.set('Pragma', 'no-cache');
-//   next();
-// });
-// app.disable('x-powered-by');
-
-// Configure routes
+// Configure server routes
 app.get('/config.json', require('./routes/config'));
 app.post('/execute', require('./routes/execute'));
 app.post('/publish', require('./routes/publish'));
 app.post('/validate', require('./routes/validate'));
 
-// Configure routes to static files
+// Configure client route
 app.use(express.static(`${__dirname}/dist/`));
 
 // Start server
-const port = process.env.PORT || 8081;
-
-app.listen(port, () => {
-  logger.info(`[app.js] Listening on port ${port}`);
+app.listen(port, (error) => {
+  if (error) {
+    logger.error('[app.js] error :', error);
+  } else {
+    logger.info(`[app.js] success : ${port}`);
+  }
 });

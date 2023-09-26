391204ac3c2536e91962ef30b9bc4478c683013a
Yusri <yusrimathews@gmail.com>
2023-09-26 17:54:16 +0700

    Updated "jbSchema" object data for testing

--- src/data/jbSchema.js
+++ src/data/jbSchema.js
@@ -4,37 +4,28 @@ const jbSchema = {
       key: "Event.DEAudience-ee51a166-8c63-2436-fcf8-91c30358bee1.Entity_ID",
       name: "Entity_ID",
       type: "Text",
-      length: 500,
-      default: null,
-      isNullable: null,
-      isPrimaryKey: null
+      length: 500
     },
     {
       key: "Event.DEAudience-ee51a166-8c63-2436-fcf8-91c30358bee1.Email",
       name: "Email",
       type: "EmailAddress",
       length: 254,
-      default: null,
-      isNullable: true,
-      isPrimaryKey: null
+      isNullable: true
     },
     {
       key: "Event.DEAudience-ee51a166-8c63-2436-fcf8-91c30358bee1.Mobile",
       name: "Mobile",
       type: "Phone",
       length: 50,
-      default: null,
-      isNullable: true,
-      isPrimaryKey: null
+      isNullable: true
     },
     {
       key: "Event.DEAudience-ee51a166-8c63-2436-fcf8-91c30358bee1.Field Name",
       name: "Field Name",
       type: "Text",
       length: 50,
-      default: null,
-      isNullable: true,
-      isPrimaryKey: null
+      isNullable: true
     }
   ]
 }

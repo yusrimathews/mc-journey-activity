62ae3642a582c68ced8f6a1031b5686699697d9b
Yusri <yusrimathews@gmail.com>
2023-09-26 17:30:02 +0700

    Added "jbEndpoints" object data for testing

---
+++ src/data/jbEndpoints.js
@@ -0,0 +1,10 @@
+const jbEndpoints = {
+  restHost: "rest.s50.exacttarget.com",
+  stackKey: "S50",
+  stackHost: "mc.s50.exacttarget.com",
+  ssoUrl: "https://mc.s50.exacttarget.com/cloud/tools/SSO.aspx?env=default&legacy=1&sk=S50",
+  fuelapiRestHost: "https://www-mc-s50.exacttargetapis.com/",
+  authTSSD: "https://mc8bjsqf9zy3-78utw1plm5dv2gh.auth.marketingcloudapis.com"
+}
+
+module.exports = jbEndpoints;

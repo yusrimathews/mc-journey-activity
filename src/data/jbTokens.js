55535aeeb65538e4d4ad80719730dbcb117db0d6
Yusri <yusrimathews@gmail.com>
2023-09-26 17:29:48 +0700

    Added "jbTokens" object data for testing

---
+++ src/data/jbTokens.js
@@ -0,0 +1,11 @@
+const jbTokens = {
+  token: "xKuY2RmV87ia1cNqPwJbG-zD6UXxlhTQeO7P3RoVSi_5ntfZAmKvE4GReI0cOtzrDnQxO2mXwP_hNPEYd7N5uQCAlcR-RljVCie7aKTygBFAGe9J9kOAMK1TPRtOhFvP-j3N6JDR9IKa_c0kCzhUOFD7qQp-2YkcqUMV7HoPKExMgqrOsXB__HToWynKCSKoDeL0AI6hb3d4iLzwPnRIu5vXlWRehSbI9AGT4cCx0uzOcTRuGqoijDdD_gMpLkQu2iIrxuT0stBV6Yj-_gnz8NFhYpRo7heU6mFZvqjeNhwa7R2PYPO-xMDE5BKKF0K2m_h0EoxKm4Ig5qeR2ydz1",
+  fuel2token: "HwPqBDFLzXnPWo7SaKTeUvZR",
+  expires: 1695639774560,
+  stackKey: "S50",
+  EID: 500000000,
+  MID: 500000000,
+  UID: 951827403
+}
+
+module.exports = jbTokens;
